c6b0fd33253b39c11d2d7fbe060c33aa29d77189
Rich Elton <richelton14@gmail.com>
2018-09-06 15:25:43 -0300

    Roteiro 01 - Ordenação(Bubble, Insertion, Selection e Simultaneous)

---
+++ R01-01-Rot-SimpleSorting-Simultaneous-Bubble-environment/src/main/java/sorting/simpleSorting/BubbleSort.java
@@ -0,0 +1,24 @@
+package sorting.simpleSorting;
+
+import sorting.AbstractSorting;
+
+import util.*;
+
+/**
+ * The bubble sort algorithm iterates over the array multiple times, pushing big
+ * elements to the right by swapping adjacent elements, until the array is
+ * sorted.
+ */
+public class BubbleSort<T extends Comparable<T>> extends AbstractSorting<T> {
+	
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		for (int j = leftIndex; j < rightIndex-1; j++) {
+			for (int i = leftIndex; i < rightIndex; i++) {
+				if (array[i].compareTo(array[i+1]) > 0) {
+					Util.swap(array, i, i+1);
+				}
+			}
+		}
+	}
+}
---
+++ R01-01-Rot-SimpleSorting-Simultaneous-Bubble-environment/src/main/java/sorting/simpleSorting/InsertionSort.java
@@ -0,0 +1,27 @@
+package sorting.simpleSorting;
+
+import sorting.AbstractSorting;
+import util.Util;
+
+/**
+ * As the insertion sort algorithm iterates over the array, it makes the
+ * assumption that the visited positions are already sorted in ascending order,
+ * which means it only needs to find the right position for the current element
+ * and insert it there.
+ */
+public class InsertionSort<T extends Comparable<T>> extends AbstractSorting<T> {
+	
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		 for (int i = leftIndex + 1; i <= rightIndex; i++) {
+			if (array[i].compareTo(array[i-1]) < 0) {
+				Util.swap(array, i, i-1);
+			}
+			int j = i - 1;
+			while ((j > 0) && (array[j].compareTo(array[j-1]) < 0)) {
+				Util.swap(array, j, j-1);
+				j--;
+			}
+		}
+	}
+}
