f184e27e6c1576cb170057cee5882c98bb637da6
Rich Elton <richelton14@gmail.com>
2018-10-15 13:59:02 -0300

    Roteiro 08 implementado

---
+++ R08-01-Rot-TabelaHash-EndFechado-environment/src/main/java/adt/hashtable/closed/HashtableClosedAddressImpl.java
@@ -0,0 +1,114 @@
+package adt.hashtable.closed;
+
+import java.util.LinkedList;
+import java.util.Iterator;
+
+
+import adt.hashtable.hashfunction.HashFunction;
+import adt.hashtable.hashfunction.HashFunctionClosedAddress;
+import adt.hashtable.hashfunction.HashFunctionClosedAddressMethod;
+import adt.hashtable.hashfunction.HashFunctionFactory;
+import util.Util;
+
+public class HashtableClosedAddressImpl<T> extends AbstractHashtableClosedAddress<T> {
+
+   /**
+    * A hash table with closed address works with a hash function with closed
+    * address. Such a function can follow one of these methods: DIVISION or
+    * MULTIPLICATION. In the DIVISION method, it is useful to change the size
+    * of the table to an integer that is prime. This can be achieved by
+    * producing such a prime number that is bigger and close to the desired
+    * size.
+    * 
+    * For doing that, you have auxiliary methods: Util.isPrime and
+    * getPrimeAbove as documented bellow.
+    * 
+    * The length of the internal table must be the immediate prime number
+    * greater than the given size (or the given size, if it is already prime). 
+    * For example, if size=10 then the length must
+    * be 11. If size=20, the length must be 23. You must implement this idea in
+    * the auxiliary method getPrimeAbove(int size) and use it.
+    * 
+    * @param desiredSize
+    * @param method
+    */
+
+   @SuppressWarnings({ "rawtypes", "unchecked" })
+   public HashtableClosedAddressImpl(int desiredSize, HashFunctionClosedAddressMethod method) {
+      int realSize = desiredSize;
+
+      if (method == HashFunctionClosedAddressMethod.DIVISION) {
+         realSize = this.getPrimeAbove(desiredSize); // real size must the
+         // the immediate prime
+         // above
+      }
+      initiateInternalTable(realSize);
+      HashFunction function = HashFunctionFactory.createHashFunction(method, realSize);
+      this.hashFunction = function;
+   }
+
+   // AUXILIARY
+   /**
+    * It returns the prime number that is closest (and greater) to the given
+    * number.
+    * If the given number is prime, it is returned. 
+    * You can use the method Util.isPrime to check if a number is
+    * prime.
+    */
+   int getPrimeAbove(int number) {
+      int i = number+1;
+      boolean continua = true;
+      
+      while(continua) {
+    	  if (Util.isPrime(i)) {
+    		  continua = false;
+    	  } else {
+    		  i++;
+    	  }
+      }
+      return i;
+   }
+
+   @Override
+   public void insert(T element) {
+	   LinkedList<T> lista = (LinkedList<T>) this.table[((HashFunctionClosedAddress<T>) this.hashFunction).hash(element)];
+	   if (lista == null) {
+		   this.table[((HashFunctionClosedAddress<T>) this.hashFunction).hash(element)] = new LinkedList<T>();
+	   } else {
+		   this.COLLISIONS++;
+	   }
+	   if (!((LinkedList<T>) this.table[((HashFunctionClosedAddress<T>) this.hashFunction).hash(element)]).contains(element)) {
+		   ((LinkedList<T>)this.table[((HashFunctionClosedAddress<T>) this.hashFunction).hash(element)]).add(element);
+	   }
+	   this.elements++;
+   }
+
+   @Override
+   public void remove(T element) {
+      ((LinkedList<T>) this.table[((HashFunctionClosedAddress<T>) this.hashFunction).hash(element)]).remove(element);   
+      this.elements--;
+   }
+
+   @Override
+   public T search(T element) {
+      T saida = null;
+      if (((LinkedList<T>) this.table[((HashFunctionClosedAddress<T>) this.hashFunction).hash(element)]) != null) {
+    	  if (((LinkedList<T>) this.table[((HashFunctionClosedAddress<T>) this.hashFunction).hash(element)]).contains(element)) {
+    		  saida = element;
+    	  }
+      }
+      return saida;
+   }
+
+   @Override
+   public int indexOf(T element) {
+       int res  = -1;
+       if (((LinkedList<T>) this.table[((HashFunctionClosedAddress<T>) this.hashFunction).hash(element)]) != null) {
+    	   if (((LinkedList<T>) this.table[((HashFunctionClosedAddress<T>) this.hashFunction).hash(element)]).contains(element)) {
+    		   res = ((HashFunctionClosedAddress<T>) this.hashFunction).hash(element);
+    	   }
+       }
+       return res;
+   }
+
+}
